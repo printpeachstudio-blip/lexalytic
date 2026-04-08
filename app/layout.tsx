12055b362df3448c255fa6608916957103fc00a3
printpeachstudio-blip <printpeachstudio@gmail.com>
2026-04-09 00:07:19 +0100

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Lexalytic | Turn Your Data Into Decisions',
@@ -28,7 +29,21 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FWP3KBJ211"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FWP3KBJ211');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   )
 }
