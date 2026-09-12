2513f6aee91cac9204798545c88f0599c817902b
Mihir Hindocha <hello@lexalytic.com>
2026-09-12 19:46:24 +0100

    tidy layout indentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,7 +100,7 @@ export default function RootLayout({
       </head>
       <body>
         {children}
-              <Analytics />
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
